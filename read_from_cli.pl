5239466320bb1d27bebe5be2be31a78a9a0e27c0
Gary Little <gary.j.little@gmail.com>
2018-07-01 17:03:35 -0400

    Added example reading STDIN to an array/list

--- read_from_cli.pl
+++ read_from_cli.pl
@@ -12,4 +12,21 @@ print ">>>>".$line."<<<<\n";
 chomp($line = <STDIN>);
 print "XXXX".$line."XXXX\n";
 
+#To read many lines, send STDIN to a list - use EOF to terminate.
+#
+print "Enter many lines with <CR>, use CTRL-D to send EOF\n";
+@lines = <STDIN>;
+print "Lines read\n";
+foreach $line (@lines) {
+    print $line;
+}
+print "Enter many lines with <CR>, use CTRL-D to send EOF\n";
+# This idiom removed newlines from each "line".  
+chomp(@lines = <STDIN>);
+print "Lines read\n";
+foreach $line (@lines) {
+    print $line;
+}
+
+
 
